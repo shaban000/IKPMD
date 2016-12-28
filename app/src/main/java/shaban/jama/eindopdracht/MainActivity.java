4c2bd0580b9914e80ef0fe904b8cb5443cdf75d4
sangam <sangam_nepali@hotmail.com>
2016-12-28 22:21:52 +0100

    main menu

--- app/src/main/java/shaban/jama/eindopdracht/MainActivity.java
+++ app/src/main/java/shaban/jama/eindopdracht/MainActivity.java
@@ -1,13 +1,47 @@
 package shaban.jama.eindopdracht;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton leren;
+    ImageButton status;
+    ImageButton toevoegen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        leren = (ImageButton)findViewById(R.id.leren_btn);
+        status = (ImageButton)findViewById(R.id.status_btn);
+        toevoegen = (ImageButton)findViewById(R.id.toevoegen_btn);
+
+        leren.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Leren.class);
+                startActivity(intent);
+            }
+        });
+        status.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Status.class);
+                startActivity(intent);
+            }
+        });
+        toevoegen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Toevoegen.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/shaban/jama/eindopdracht/Toevoegen.java
@@ -0,0 +1,13 @@
+package shaban.jama.eindopdracht;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Toevoegen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toevoegen);
+    }
+}
