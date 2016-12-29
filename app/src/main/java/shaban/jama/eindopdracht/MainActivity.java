0db3279686146951b099313e31ed774ee33028b3
Shaban Jama <Shaban@MacBook-Air-van-Shaban.local>
2016-12-29 23:02:37 +0100

    Aanpassing mainmenu

--- app/src/main/java/shaban/jama/eindopdracht/MainActivity.java
+++ app/src/main/java/shaban/jama/eindopdracht/MainActivity.java
@@ -1,47 +1,106 @@
 package shaban.jama.eindopdracht;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    ImageButton leren;
-    ImageButton status;
-    ImageButton toevoegen;
+    private PieChart mChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        leren = (ImageButton)findViewById(R.id.leren_btn);
-        status = (ImageButton)findViewById(R.id.status_btn);
-        toevoegen = (ImageButton)findViewById(R.id.toevoegen_btn);
+        int hoogte = 150;
+        int duration = 1000;
 
-        leren.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),Leren.class);
-                startActivity(intent);
-            }
-        });
-        status.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),Status.class);
-                startActivity(intent);
-            }
-        });
-        toevoegen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),Toevoegen.class);
-                startActivity(intent);
-            }
-        });
+        TextView leren = (TextView) findViewById(R.id.textView1);
+        leren.setY(hoogte * 3);
+        leren.animate().translationY(0);
+        leren.animate().setDuration(duration * 2 );
+        leren.animate();
+
+        TextView toevoegen = (TextView) findViewById(R.id.textView2);
+        toevoegen.setY(hoogte * 2);
+        toevoegen.animate().translationY(0);
+        toevoegen.animate().setDuration(duration + duration / 2);
+        toevoegen.animate();
+
+        TextView instellingen = (TextView) findViewById(R.id.textView3);
+        instellingen.setY(hoogte);
+        instellingen.animate().translationY(0);
+        instellingen.animate().setDuration(duration);
+        instellingen.animate();
+
+
+        mChart = (PieChart) findViewById(R.id.chart);
+        mChart.setDescription("Status");
+        mChart.setTouchEnabled(true);
+        mChart.setDrawSliceText(true);
+        mChart.getLegend().setEnabled(false);
+        mChart.setTransparentCircleColor(Color.rgb(130, 130, 130));
+        mChart.animateY(2000, Easing.EasingOption.EaseInQuart);
+
+        setdata(); // tijdelijke voorbeeld data.
     }
+
+    private void setdata() {
+        int aantal = 80;
+        int currentEcts = aantal;
+        ArrayList<Entry> yValues = new ArrayList<>();
+        ArrayList<String> xValues = new ArrayList<>();
+
+        yValues.add(new Entry(aantal, 0));
+        xValues.add(getResources().getString(R.string.Gedaan));
+
+        yValues.add(new Entry(100 - currentEcts, 1));
+        xValues.add(getResources().getString(R.string.Tedoen));
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        if (currentEcts <10) {
+            colors.add(Color.rgb(244,81,30));
+        } else if (currentEcts < 40){
+            colors.add(Color.rgb(235,0,0));
+        } else if  (currentEcts < 50) {
+            colors.add(Color.rgb(253,216,53));
+        } else {
+            colors.add(Color.rgb(67,160,71));
+        }
+        colors.add(Color.rgb(255,0,0));
+
+        PieDataSet dataSet = new PieDataSet(yValues, "ECTS");
+        dataSet.setColors(colors);
+
+        PieData data = new PieData(xValues, dataSet);
+        mChart.setData(data);
+    }
+
+    public void Status(View view){
+        startActivity(new Intent(getApplicationContext(),Status.class));
+    }
+
+    public void Leren(View view){
+        startActivity(new Intent(getApplicationContext(),Leren.class));
+    }
+
+    public void Toevoegen(View view){
+        startActivity(new Intent(getApplicationContext(),Toevoegen.class));
+    }
+
+    public void Instellingen(View view){
+        startActivity(new Intent(getApplicationContext(),Toevoegen.class)); // View moet nog gemaakt worden.
+    }
+
 }
