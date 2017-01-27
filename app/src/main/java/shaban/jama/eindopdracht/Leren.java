82d8dfcccc5867c1d177ac5a0a6b59a5c7f60436
sangam <sangam_nepali@hotmail.com>
2017-01-27 14:51:18 +0100

    final push Database/swiper small rename v2

--- app/src/main/java/shaban/jama/eindopdracht/Leren.java
+++ app/src/main/java/shaban/jama/eindopdracht/Leren.java
@@ -1,15 +1,27 @@
 package shaban.jama.eindopdracht;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
+import shaban.jama.eindopdracht.Adapter.Week_lijst_adapter;
+import shaban.jama.eindopdracht.Database.DatabaseHelper;
+import shaban.jama.eindopdracht.Database.DatabaseInfo;
+import shaban.jama.eindopdracht.model.Subdoel;
 
 
 public class Leren extends AppCompatActivity {
-    TextView week1;
+    private TextView week1;
+    private ListView weken_lijst;
+    private Week_lijst_adapter weekAdapter;
+    ArrayList<Subdoel> subdoelen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
