36efd2d1bf8212fd4b4242d9bd5ad9708b4049a4
Shaban Jama <Shaban@dhcp-145.101.74.227.pool.hsleiden.nl>
2017-01-27 15:43:22 +0100

    Merge samen toch

--- app/src/main/java/shaban/jama/eindopdracht/model/Subdoel.java
+++ app/src/main/java/shaban/jama/eindopdracht/model/Subdoel.java
@@ -1,12 +1,55 @@
 package shaban.jama.eindopdracht.Model;
 
-
 /**
- * Created by Shaban on 05-01-17.
+ * Created by sangam on 30/12/2016.
  */
 
 public class Subdoel {
+    private int id;
     private String naam;
-    private int week;
-    private boolean voldaan;
+    private String week;
+    private String voldaan;
+    private String fk_id_leerdoel;
+
+    public Subdoel(){
+
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getFk_id_leerdoel() {
+        return fk_id_leerdoel;
+    }
+
+    public void setFk_id_leerdoel(String fk_id_leerdoel) {
+        this.fk_id_leerdoel = fk_id_leerdoel;
+    }
+
+    public String getVoldaan() {
+        return voldaan;
+    }
+
+    public void setVoldaan(String voldaan) {
+        this.voldaan = voldaan;
+    }
+
+    public String getWeek() {
+        return week;
+    }
+
+    public void setWeek(String week) {
+        this.week = week;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
 }
